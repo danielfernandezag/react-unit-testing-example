c1330fe4f75efda7be2ea1d0d6dbf91f1e026157
Julio Daniel Fernandez Aguirre <danielfernandezaguirre@outlook.com>
2019-02-22 11:06:37 -0600

    coverage reports and package.json changes

--- src/redux/reducers/index.test.js
+++ src/redux/reducers/index.test.js
@@ -5,7 +5,7 @@ describe('when configureStore runs', () => {
 		const store = configureStore();
 		expect(store.getState()).toMatchObject({
 			balanceState: { balance: 0 },
-			listState: { list: [{ cost: 1200, id: '1', name: 'uno' }, { cost: 2100, id: '2', name: 'dos' }, { cost: 1100, id: '3', name: 'tres' }, { cost: 200, id: '4', name: 'cuatro' }] }
+			listState: { list: [] }
 		});
 	});
 });
--- src/redux/reducers/listReducer.test.js
+++ src/redux/reducers/listReducer.test.js
@@ -1,20 +1,20 @@
 import listState from './listReducer';
 import * as Actions from '../actions/index';
 
-const list = [{ id: '1', name: 'uno', cost: 1200 }, { id: '2', name: 'dos', cost: 2100 }, { id: '3', name: 'tres', cost: 1100 }, { id: '4', name: 'cuatro', cost: 200 }];
+const mockList = [{ id: '1', name: 'one', cost: 100 }, { id: '2', name: 'two', cost: 200 }];
 
 describe('list reducer', () => {
 	it('should return the initial state', () => {
 		expect(listState(undefined, {})).toEqual({
-			list
+			list: []
 		});
 	});
 });
 
 it('should handle ADD_TO_LIST', () => {
-	expect(listState({ list }, Actions.addToList({ id: '5', name: 'cinco', cost: 500 }))).toEqual({ list: [...list, { id: '5', name: 'cinco', cost: 500 }] });
+	expect(listState({ list: mockList }, Actions.addToList({ id: '3', name: 'three', cost: 300 }))).toEqual({ list: [...mockList, { id: '3', name: 'three', cost: 300 }] });
 });
 
 it('should handle REMOVE_FROM_LIST', () => {
-	expect(listState({ list }, Actions.removeFromList(1))).toEqual({ list: [{ id: '1', name: 'uno', cost: 1200 }, { id: '3', name: 'tres', cost: 1100 }, { id: '4', name: 'cuatro', cost: 200 }] });
+	expect(listState({ list: mockList }, Actions.removeFromList(1))).toEqual({ list: [{ id: '1', name: 'one', cost: 100 }] });
 });
